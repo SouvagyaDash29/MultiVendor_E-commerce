fee211eecd95295447215192eadeab3bb3a3c7ab
SouvagyaDash29 <souvagyadash1999@gmail.com>
2024-02-22 23:36:40 +0530

    Modifify Header section

--- Frontend/src/Customer/components/Header/Header.jsx
+++ Frontend/src/Customer/components/Header/Header.jsx
@@ -1,47 +1,30 @@
-import React from "react";
-import { MdSearch } from "react-icons/md";
-import { IoCartOutline } from "react-icons/io5";
-import { FaRegUserCircle } from "react-icons/fa";
-import "./Header.css";
-import { IoIosArrowDown } from "react-icons/io";
-import { MdStorefront } from "react-icons/md";
-import { NavLink } from "react-router-dom";
-// import Navbar from "./Navbar";
+import React from 'react';
+import './Header.css';
+import LoGo from '../../../assets/Quick_Mart_Logo.png';
+import { FiSearch } from "react-icons/fi";
+import { HiOutlineHeart } from "react-icons/hi2";
+import { RiShoppingCartLine } from "react-icons/ri";
+import { BiUser } from "react-icons/bi";
 
 const Header = () => {
   return (
     <>
-      <header className="header">
-        <div className="logo">
-          <NavLink to="/">
-            <img src="../assets/Quick_Mart_Logo.png" alt="img" />
-          </NavLink>
-        </div>
-        <div className="search">
-          <input
-            type="search"
-            name="text"
-            id="search-box"
-            placeholder="Search for Products"
-          />
-          <MdSearch fontSize="1.6vw" className="search-btn" />
-        </div>
-        <div className="login-section">
-          <FaRegUserCircle fontSize="1.2vw" />
-          Login
-          <IoIosArrowDown fontSize="1.2vw" className="login-arrow" />
-        </div>
-        <div className="cart-btn">
-          <IoCartOutline fontSize="1.8vw" />
-          Cart
-        </div>
-        <div className="seller-section">
-          <MdStorefront fontSize="2vw" />
-          Become a Seller
-        </div>
-      </header>
+    <div className='header'>
+    <div className="logo-section">
+      <img src= {LoGo} alt="icon" />
+    </div>
+    <div className="search-box">
+      <input type="text" placeholder='Search '/> <FiSearch />
+    </div>
+    <div className="icon-section">
+    <HiOutlineHeart />
+    <RiShoppingCartLine />
+    <BiUser />
+    </div>
+    </div>
+    <hr />
     </>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
