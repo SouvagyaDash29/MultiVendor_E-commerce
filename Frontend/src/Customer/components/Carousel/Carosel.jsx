2da974e0a98723d8908e6cd85cf78039fbd87678
SouvagyaDash29 <souvagyadash1999@gmail.com>
2024-02-24 17:47:36 +0530

    change slider style and images

--- Frontend/src/Customer/components/Carousel/Carosel.jsx
+++ Frontend/src/Customer/components/Carousel/Carosel.jsx
@@ -3,27 +3,20 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Carosel.css";
-import CSimg1 from "../../../assets/home-section-carosel/Green Simple Fashion Sale Promotion Banner Landscape 1.webp";
-import CSimg2 from "../../../assets/home-section-carosel/Blue and Purple Gradient Laptop Sale Banner (3) 1.webp";
-// import CSimg3 from "../../../assets/Carosel1/16136135_5691822.webp";
-// import CSimg4 from "../../../assets/Carosel1/Blue_and_Purple_Gradient_Laptop_Sale_Banner_7.WEBP";
-// import CSimg5 from "../../../assets/home-section-carosel/Green and Black Vivid Bold Blocks Electronics and Appliances Banner.webp";
-// import CSimg6 from "../../../assets/Carosel2/Blue and Purple Gradient Laptop Sale Banner (8).webp";
-// import CSimg7 from "../../../assets/Carosel2/Blue and Purple Gradient Laptop Sale Banner (9).webp";
-// import CSimg8 from "../../../assets/Carosel3/Blue and Purple Gradient Laptop Sale Banner (6).webp";
-import CSimg9 from "../../../assets/home-section-carosel/Blue and Purple Gradient Laptop Sale Banner (5) 1.webp";
-import CSimg10 from "../../../assets/home-section-carosel/Blue and Purple Gradient Laptop Sale Banner (1) 1.webp";
+import sliderImg1 from '../../../assets/home-section-carosel/Tv_1.webp';
+import sliderImg2 from '../../../assets/home-section-carosel/Home1.webp';
+import sliderImg3 from '../../../assets/home-section-carosel/phone_5.jpg';
+import sliderImg4 from '../../../assets/home-section-carosel/phone_4.jpg';
+import sliderImg5 from '../../../assets/home-section-carosel/phone_1.webp';
 
 const settings = {
-  // dots: true,
+  dots: true,
   fade: true,
   infinite: true,
   speed: 800,
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
-  // nextArrow: <IoIosArrowBack />,
-  // prevArrow: <IoIosArrowForward />,
   autoplay: true,
   autoplaySpeed: 4000,
 };
@@ -35,31 +28,18 @@ const Carosel = () => {
           
             <Slider {...settings} className="slider-left">
 
-              <img src={CSimg1} alt="img" />
+              <img src={sliderImg1} alt="img" />
               
-              <img src={CSimg2} alt="img" />
+              <img src={sliderImg2} alt="img" />
               
-              {/* <img src={CSimg5} alt="img" /> */}
+              <img src={sliderImg3} alt="img" />
               
-              <img src={CSimg9} alt="img" />
+              <img src={sliderImg4} alt="img" />
               
-              <img src={CSimg10} alt="img" />
+              <img src={sliderImg5} alt="img" />
             </Slider>
           </div>
           
-          {/* <div className="slider-right">
-            <Slider {...settings} className="slider-right-up">
-              <img src={CSimg5} alt="icon" />
-              <img src={CSimg6} alt="icon" />
-              <img src={CSimg7} alt="icon" />
-            </Slider>
-            <Slider {...settings} className="slider-right-down">
-              <img src={CSimg8} alt="icon" />
-              <img src={CSimg9} alt="icon" />
-              <img src={CSimg10} alt="icon" />
-            </Slider>
-          </div>
-        </div> */}
       </div>
     </>
   );
