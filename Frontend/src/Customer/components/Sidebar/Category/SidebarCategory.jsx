4dd587fff4866e325e0de40905bfa7af53c39f33
SouvagyaDash29 <souvagyadash1999@gmail.com>
2024-03-02 10:33:29 +0530

    Add Category filter in sidebar

--- Frontend/src/Customer/components/Sidebar/Category/SidebarCategory.jsx
+++ Frontend/src/Customer/components/Sidebar/Category/SidebarCategory.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './SidebarCategory.css'
+import Input from '../../Input/Input'
+
 
 const SidebarCategory = () => {
   return (
-    <div>Category</div>
+    <div>
+      <h2 className='sidebar-title'> Category</h2>
+      <div className='sidebar-label'>
+        <Input/>
+        <Input/>
+        <Input/>
+        <Input/>
+      </div>
+    </div>
   )
 }
 
