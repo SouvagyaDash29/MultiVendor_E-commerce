b74f2f6745100c7fd98fbe991960197cdba603fa
SouvagyaDash29 <souvagyadash1999@gmail.com>
2024-03-02 10:35:25 +0530

    Add Color filter in sidebar

--- Frontend/src/Customer/components/Sidebar/Colors/Colors.jsx
+++ Frontend/src/Customer/components/Sidebar/Colors/Colors.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import './Colors.css';
+import Input from "../../Input/Input";
 
 const Colors = () => {
   return (
-    <div>Colors</div>
-  )
-}
+    <div>
+      <h2 className="sidebar-title color-title">Colors</h2>
 
-export default Colors
+      <div className="sidebar-label">
+       <Input />
+       <Input />
+       <Input />
+       <Input />
+      </div>
+    </div>
+  );
+};
+
+export default Colors;
