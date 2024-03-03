64fe5e086863351dce772f1a026cccfa24167619
SouvagyaDash29 <souvagyadash1999@gmail.com>
2024-03-03 19:19:59 +0530

    Rename folder name SidebarCategory to Category

---
+++ Frontend/src/Customer/components/Sidebar/Category/Category.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './Category.css'
+import Input from '../../Input/Input'
+
+
+const Category = (handleChange) => {
+  return (
+    <div>
+      <h2 className='sidebar-title'> Category</h2>
+      <div className='sidebar-label'>
+        <lable className="sidebar-label-container">
+          <input onChange={handleChange} type="radio" value="" name="test" />
+          <span className='checkmark'></span> All
+        </lable>
+
+
+        <Input 
+        handleChange={handleChange}
+        value="sneakers"
+        title="Sneakers"
+        name="test"
+        />
+        <Input 
+        handleChange={handleChange}
+        value="flats"
+        title="Flats"
+        name="test"
+        />
+        <Input 
+        handleChange={handleChange}
+        value="sandals"
+        title="Sandals"
+        name="test"
+        />
+        <Input 
+        handleChange={handleChange}
+        value="heals"
+        title="Heals"
+        name="test"
+        />
+
+      </div>
+    </div>
+  )
+}
+
+export default Category
