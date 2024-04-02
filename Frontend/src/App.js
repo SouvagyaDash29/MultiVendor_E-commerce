4227acd39ce1a194944a7f8095d676b23b20bd1f
subhankar <subhankarbehera48@gmail.com>
2024-04-02 15:38:16 +0530

    Login and signup page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
 
 import Buyer from "./Customer/Buyer";
+// import LoginPage from './auth/pages/Login/LoginPage';
+// import Login from "./auth/pages/login/Login";
 // import Footer from "./Customer/components/Footer/Footer";
-import Vendor from "./VENDOR/Vendor";
-
-
+// import Login from './auth/pages/login/Login';
+// import login from './auth/pages/login/Login';
+import LoginPage from './auth/pages/Login/LoginPage';
 
 function App() {
   return (
-    <div >
-         
+    <div >   
      {/* <Vendor/> */}
-      <Buyer />
-
+      {/* <Buyer /> */}
+      <LoginPage/>
     </div>
   );
 }
