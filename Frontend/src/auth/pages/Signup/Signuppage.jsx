f30e91d9d2d0f3a101bfe70e7cc16e05a2375826
subhankar <subhankarbehera48@gmail.com>
2024-04-18 16:10:15 +0530

    removed bootstrap

--- Frontend/src/auth/pages/Signup/Signuppage.jsx
+++ Frontend/src/auth/pages/Signup/Signuppage.jsx
@@ -9,29 +9,29 @@ const Signuppage = () => {
         <div class="child1">
             <img id='simg' src={signupphoto} alt="" height="500px " width="500px"/>
         </div>
-        <div >
-           <h3 class="mt-5" id='heading' >Create Your Account</h3>
+        <div id='customerDetails'>
+           <h3 class="" id='heading' >Create Your Account</h3>
            <form action="">
-           <div class="card-body col-md-12" style={{marginRight:'60%'}}>
-            <label for="" class="mb-2 mt-2">Name</label>
-            <input type="text" class="form-control"/>
-            <label for="" class="mb-2 mt-2">Email</label>
-            <input type="email" class="form-control"/>
-            <label for="" class="mb-2 mt-2">Role</label>
-            <select class="form-control">
+           <div class="" style={{marginRight:'60%'}}>
+            <label for="" class="">Name</label>
+            <input type="text" class=""/>
+            <label for="" class="">Email</label>
+            <input type="email" class=""/>
+            <label for="" class="">Role</label>
+            <select class="">
             <option value="customer">Customer</option>
             <option value="vendor">Vendor</option>
             </select>
-            <label for="" class="mb-2 mt-2">Password</label>
-            <input type="password" class="form-control"/>
-            <label for="" class="mb-2 mt-2">Confirm Password</label>
-            <input type="password" class="form-control"/>
+            <label for="" class="">Password</label>
+            <input type="password" class=""/>
+            <label for="" class="">Confirm Password</label>
+            <input type="password" class=""/>
         </div>
-        <div class="card-footer text-center mt-2" style={{marginLeft:'60px'}}>
+        <div class="" style={{marginLeft:'60px'}}>
             <h6>Already have Account?? <Link to='/login'>Login here</Link></h6>
         </div>
         <div class="text-center main">
-            <input type="submit" class="btn btn-outline-primary" id="btn1"/>
+            <input type="submit" class="" id="btn1"/>
             </div>
         </form>
         </div>
